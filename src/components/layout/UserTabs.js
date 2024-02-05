729905acdd935fac62b7f60c495c3e0af44826be
dkurmaev <danilkurmayev@gmail.com>
2024-02-05 13:44:46 +0100

    menu tabs modified

--- src/components/layout/UserTabs.js
+++ src/components/layout/UserTabs.js
@@ -6,7 +6,7 @@ export default function UserTabs({ isAdmin }) {
   const path = usePathname();
   
     return (
-      <div className="flex max-w-xl items-center mx-auto justify-between gap-6 tabs">
+      <div className="flex max-w-2xl items-center mx-auto text-gray-400 justify-between gap-8 tabs">
         <Link className={path === "/profile" ? "active" : ""} href={"/profile"}>
           Profile
         </Link>
@@ -20,7 +20,7 @@ export default function UserTabs({ isAdmin }) {
             </Link>
             <Link
               href={"/menu-items"}
-              className={/menu-items/.test(path) ? "active" : ""}
+              className={/menu-items/.test(path) ? "active" : "" }
             >
               Menüartikel
             </Link>
