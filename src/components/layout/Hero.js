e67979330fd3a7a51fc6257f453c484db1eb0921
dkurmaev <danilkurmayev@gmail.com>
2024-02-09 00:08:49 +0100

    hero burger mobil

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -6,16 +6,28 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="hero md:mt-4 ">
+    <section className="hero md:mt-4">
       <div className=" py-8 md:py-12 ">
         <h1 className="text-2xl font-semibold mt-2 text-center leading-normal">
           SAFTIGSTE&nbsp;
           <br />
           <span className="text-primary text-6xl">SPACEBOX BURGER </span>
           <br />
+          <Link href={"/menu"}>
+            <div className="relative pt-16 pb-1 hover:scale-110 transform transition-transform duration-300 ease-in-out block md:hidden">
+              <Image
+                className="animate-gravitation"
+                src={"/img/space.png"}
+                width={400}
+                height={400}
+                style={{ objectFit: "contain" }}
+                alt={"burger"}
+              />
+            </div>
+          </Link>
           IN BERNAU BEI BERLIN
         </h1>
-        <p className="my-6 text-gray-500 text-center">
+        <p className="my-6 text-gray-500 text-center  ">
           Entdecke den Saftigkeit unserer Burger! Bestelle jetzt und genieße
           erstklassiges Rindfleisch aus Deutschland und der Region Bernau bei
           Berlin. Schnell, frisch und voller Aktionen!
