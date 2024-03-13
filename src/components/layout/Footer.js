e76ff0c33768e0eac2179058ecb306dadffa6c73
Danil <114011333+dkurmaev@users.noreply.github.com>
2024-03-13 18:19:27 +0100

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -131,7 +131,7 @@ export default function Footer() {
         <span className="md:bg-grey-500 text-grey-400 p-2 rounded-md">
           Im Projekt waren&nbsp;
           <a
-            href="https://drive.google.com/file/d/1Q_LPBgyi9WTpV7PzPZNNIWZWjqzjLsVf/view?usp=sharing"
+            href="www.linkedin.com/in/danil-kurmayev-141b44272"
             target="_blank"
             className="text-primary hover:underline"
           >
